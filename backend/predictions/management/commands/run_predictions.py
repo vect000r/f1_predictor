65b3118c6fe03b24626ff66886bdaa94b356e4f9
vect0r <vectoreddd@gmail.com>
2025-09-22 22:46:43 +0200

    fix: moved prediction logic to management command

---
+++ backend/predictions/management/commands/run_predictions.py
@@ -0,0 +1,46 @@
+from django.core.management.base import BaseCommand
+from django.core.management.base import CommandError
+import logging
+from predictions import utils
+from rest_framework import status
+from drivers.models import Driver, DriverStanding
+from rest_framework.response import Response
+from datetime import datetime
+from predictions.models import Prediction
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    """Management command for running predictions"""
+
+    def handle(self, *args, **options):
+        try:
+            standings = list(DriverStanding.objects.all().values())
+
+            predictor = utils.Predictor(model_type='xgboost')
+            predictor.train(standings)
+
+            predictions = {}
+
+            for driver in Driver.objects.all():
+                pred = predictor.predict(standings, driver.driver_number)
+                if pred:
+                    prediction_obj, created = Prediction.objects.update_or_create(
+                        driver_number=driver.driver_number,
+                        predicted_position=pred['predicted_position'],
+                        predicted_points_gain=pred['predicted_points_gain'],
+                        predicted_total_points=pred['predicted_total_points'],
+                        current_position=pred['current_position'],
+                        current_points=pred['current_points'],
+                        confidence=pred['confidence']
+                    )
+                    action = f'Created prediction for driver #{driver.driver_number}' if created else f'Updated prediction for driver #{driver.driver_number}'
+                    logger.info(action)
+
+                else:
+                    logger.error(f"Error predicting for driver #{driver.driver_number}")
+
+        except Exception as e:
+            logger.error(f"Error getting predictions: {e}")
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
--- backend/predictions/models.py
+++ backend/predictions/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Prediction(models.Model):
+    """Model to store current predictions for each driver"""
+    driver_number = models.IntegerField()
+    predicted_position = models.FloatField()
+    predicted_points_gain = models.FloatField()
+    predicted_total_points = models.FloatField()
+    current_position = models.IntegerField()
+    current_points = models.IntegerField()
+    confidence = models.FloatField()
+
+    class Meta:
+        ordering = ['driver_number']
+        verbose_name_plural = 'Predictions'
+
+    def __str__(self):
+        return f'Predicted: {self.predicted_position} for driver #{self.driver_number}'
